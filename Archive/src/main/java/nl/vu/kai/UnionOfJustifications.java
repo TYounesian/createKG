69fe16579531c9abd55b3edea0be8dc55278b6df
Patrick Koopmann <p.k.koopmann@vu.nl>
2025-04-11 16:58:38 +0200

    ..

--- Archive/src/main/java/nl/vu/kai/UnionOfJustifications.java
+++ Archive/src/main/java/nl/vu/kai/UnionOfJustifications.java
@@ -4,13 +4,16 @@ import de.tu_dresden.inf.lat.evee.proofGenerators.ELKProofGenerator;
 import de.tu_dresden.inf.lat.evee.proofs.data.exceptions.ProofGenerationFailedException;
 import de.tu_dresden.inf.lat.evee.proofs.interfaces.IInference;
 import de.tu_dresden.inf.lat.evee.proofs.interfaces.IProof;
+import org.semanticweb.elk.owlapi.ElkReasonerFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.formats.TurtleDocumentFormat;
 import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -20,7 +23,7 @@ public class UnionOfJustifications {
         System.out.println();
         if (args.length == 0) {
             System.out.println("Usage: ");
-            System.out.println(UnionOfJustifications.class.getCanonicalName() + " OWL_FILE CLASS INDIVIDUAL OUTPUT_FILE");
+            System.out.println(UnionOfJustifications.class.getCanonicalName() + " OWL_FILE CLASS INDIVIDUALS_FILE OUTPUT_FILE");
             System.exit(0);
         }
 
@@ -28,12 +31,50 @@ public class UnionOfJustifications {
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
         OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File(args[0]));
 
+        OWLDataFactory factory = manager.getOWLDataFactory();
+
         ELKProofGenerator proofGenerator = new ELKProofGenerator();
         proofGenerator.setOntology(ontology);
-        OWLDataFactory factory = manager.getOWLDataFactory();
+
+        OWLReasoner reasoner = new ElkReasonerFactory().createReasoner(ontology);
+
+        OWLClass clazz = factory.getOWLClass(IRI.create(args[1]));
+
+        PrintWriter writer = new PrintWriter(new FileOutputStream(args[2]));
+
+        for(OWLIndividual individual:reasoner.instances(clazz).collect(Collectors.toSet())) {
+
+            writer.println(individual+":");
+
+            OWLOntology justification =
+                    forIndividual(factory,ontology,clazz, individual,proofGenerator,manager);
+
+            for(OWLAxiom axiom: justification.getAxioms()){
+                if(axiom instanceof OWLObjectPropertyAssertionAxiom){
+                    OWLObjectPropertyAssertionAxiom prp = (OWLObjectPropertyAssertionAxiom) axiom;
+                    writer.println(prp.getSubject()+" "+prp.getProperty()+" "+prp.getObject());
+                } else if (axiom instanceof OWLClassAssertionAxiom) {
+                    OWLClassAssertionAxiom ax = (OWLClassAssertionAxiom) axiom;
+                    writer.println(ax.getIndividual()+" rdf:type "+ax.getClassExpression());
+                }
+            }
+            writer.println();
+
+        }
+        writer.close();
+    }
+
+    private static OWLOntology forIndividual(
+            OWLDataFactory factory,
+            OWLOntology ontology,
+            OWLClass clazz,
+            OWLIndividual individual,
+            ELKProofGenerator proofGenerator,
+            OWLOntologyManager manager) throws ProofGenerationFailedException, OWLOntologyCreationException {
+
         OWLAxiom entailment = factory.getOWLClassAssertionAxiom(
-                factory.getOWLClass(IRI.create(args[1])),
-                factory.getOWLNamedIndividual(IRI.create(args[2]))
+                factory.getOWLClass(clazz),
+                individual
         );
 
         System.out.println("Computing justifications...");
@@ -51,8 +92,6 @@ public class UnionOfJustifications {
         OWLOntology just = manager.createOntology();
         just.addAxioms(result);
 
-        System.out.println("Saving...");
-
-        manager.saveOntology(just, new TurtleDocumentFormat(), new FileOutputStream(new File(args[3])));
+        return just;
     }
 }
